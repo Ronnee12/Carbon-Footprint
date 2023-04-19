ca3b6c161f69d7c0f96111b32c0c61106f5b9d05
Aaron Obodai <aaron.torgbor@ashesi.edu.gh>
2023-04-19 16:33:06 +0000

    second commit

--- ModesOfTransport.java
+++ ModesOfTransport.java
@@ -1,7 +1,7 @@
 public class ModesOfTransport{
     public static void main(String[] args) {
         System.out.println("We are team carbon footprint");
-        System.out.println("We are team carbon foootprint");
+        System.out.println("We are team carbon foooootprint");
 
     }
 }
